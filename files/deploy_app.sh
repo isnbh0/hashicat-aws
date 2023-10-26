e1e8d6ea877d762f2286b9fd89b2cfb12c514caa
John Choi <5154439+isnbh0@users.noreply.github.com>
2023-10-26 12:02:23 +0900

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  🌀🌵🚀 ACME Corporation: Ingeniously Crafted, Unpredictably Fun. Where Every Wild Plan Takes Flight! 🚀🌵🌀
   <!-- END -->
 
   </div>
